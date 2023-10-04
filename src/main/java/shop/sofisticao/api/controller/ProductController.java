dde844604b73a79dd6fa779ef3163b5a37ba134a
MateusOK <mateusribeiro-2004@hotmail.com>
2023-10-04 19:48:53 -0300

    feat: product controller

--- src/main/java/shop/sofisticao/api/controller/ProductController.java
+++ src/main/java/shop/sofisticao/api/controller/ProductController.java
@@ -1,21 +1,43 @@
 package shop.sofisticao.api.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import shop.sofisticao.api.entity.Product;
-import shop.sofisticao.api.repository.ProductRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+import shop.sofisticao.api.dto.request.ProductRequestDto;
+import shop.sofisticao.api.dto.response.ProductResponseDto;
+import shop.sofisticao.api.service.ProductService;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/products")
 public class ProductController {
 
-    private final ProductRepository productRepository;
+    private final ProductService productService;
 
-    @GetMapping("/api/products")
-    public Product getProduct(){
-        productRepository.save(new Product("nome", 5));
-        return productRepository.findItemByName("nome");
+    @PostMapping()
+    public ResponseEntity<ProductResponseDto> saveProduct(@RequestBody ProductRequestDto request, UriComponentsBuilder builder) {
+        var response = productService.saveProduct(request);
+        var uri = builder.path("/api/products/{id}").buildAndExpand(response.id()).toUri();
+        return ResponseEntity.created(uri).body(response);
+    }
+    @GetMapping()
+    public ResponseEntity<List<ProductResponseDto>> getAllProducts(){
+        var response = productService.findAllProducts();
+        return ResponseEntity.ok(response);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponseDto> getProductById(@PathVariable String id){
+        var response = productService.findProductById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/name/{name}")
+    public ResponseEntity<List<ProductResponseDto>> getProductByName(@PathVariable String name) {
+        var response = productService.findByName(name);
+        return ResponseEntity.ok(response);
+    }
 }
